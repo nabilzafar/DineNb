6f621378ec9ad8fa1befb6f767d3af57843452ae
Nabil Zafar <nabeelpc18@gmail.com>
2023-07-17 23:02:32 +0500

    nex1t commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import HeroSection from "../components/DineMarket/HeroSection";
 import Nav from "../components/DineMarket/Nav";
 import ProductSection from "../components/DineMarket/ProductSection";
 import PromotionSection from "../components/DineMarket/PromotionSection";
-import FooterSection from "@/components/FooterSection";
+import FooterSection from "@/components/DineMarket/FooterSection";
 
 export default function page() {
   return (
